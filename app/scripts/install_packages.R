bf58a247c5c3ab59e734d90c09764ae7c8bed459
Toan Phung <toan.phung@proteo.info>
2024-09-30 17:34:50 +0100

    update

---
+++ app/scripts/install_packages.R
@@ -0,0 +1,8 @@
+packages <- read.table("app/scripts/r_requirements.txt", stringsAsFactors = FALSE)
+for (i in 1:nrow(packages)) {
+  package <- packages[i, 1]
+  version <- packages[i, 2]
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+  }
+}
