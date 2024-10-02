c0c98e4d063ba5429df151a219040e10eaf53109
Toan Phung <toan.phung@proteo.info>
2024-10-02 12:58:39 +0100

    Update install_packages.R

--- app/scripts/install_packages.R
+++ app/scripts/install_packages.R
@@ -1,9 +1,21 @@
 options(repos = c(CRAN = "https://cloud.r-project.org/"))
+
+# Install BiocManager if not already installed
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+
+# Read the package list
 packages <- read.table("app/r_requirements.txt", stringsAsFactors = FALSE)
+
+# Install each package using BiocManager
 for (i in 1:nrow(packages)) {
+  if (packages[i, 1] == "BiocManager") {
+    next
+  }
   package <- packages[i, 1]
   version <- packages[i, 2]
   if (!require(package, character.only = TRUE)) {
-    install.packages(package, dependencies = TRUE)
+    BiocManager::install(package, version = version, dependencies = TRUE)
   }
 }
