6f4179ac5a05feca0f39fa25415dbf1ac3aaf8cb
Toan Phung <toan.phung@proteo.info>
2024-10-02 13:58:12 +0100

    update

--- app/scripts/install_packages.R
+++ app/scripts/install_packages.R
@@ -8,21 +8,41 @@ if (!requireNamespace("BiocManager", quietly = TRUE)) {
 # Read the package list
 packages <- read.table("app/r_requirements.txt", stringsAsFactors = FALSE)
 
-# Install each package using BiocManager
+# Attempt to install all packages using BiocManager
 for (i in 1:nrow(packages)) {
-  if (packages[i, 1] == "BiocManager") {
-    next
-  }
   package <- packages[i, 1]
   version <- packages[i, 2]
   if (!require(package, character.only = TRUE)) {
-      tryCatch(
-        {
-          BiocManager::install(package, version = version, dependencies = TRUE)
-        },
-        error = function(e) {
-          install.packages(package, version = version, dependencies = TRUE)
-        }
-      )
+    tryCatch(
+      {
+        message(paste("Installing", package, "version", version, "using BiocManager"))
+        BiocManager::install(package, version = version, dependencies = TRUE)
+      },
+      error = function(e) {
+        message(paste("Failed to install", package, "using BiocManager:", e$message))
+      }
+    )
+  }
+}
+
+# Check which packages are still not installed
+not_installed <- c()
+for (i in 1:nrow(packages)) {
+  package <- packages[i, 1]
+  if (!require(package, character.only = TRUE)) {
+    not_installed <- c(not_installed, package)
+  }
+}
+
+# Attempt to install the remaining packages using install.packages
+for (package in not_installed) {
+  tryCatch(
+    {
+      message(paste("Attempting to install", package, "using install.packages"))
+      install.packages(package, dependencies = TRUE)
+    },
+    error = function(e) {
+      message(paste("Failed to install", package, "using install.packages:", e$message))
     }
+  )
 }
